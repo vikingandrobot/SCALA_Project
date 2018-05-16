0231e5442b83f935ec589fe97c05778652cecb09
ripwin <sathiya.kirushnapillai@heig-vd.ch>
2018-05-16 17:10:02 +0200

    Add play base

---
+++ dev/docker/images/mysql/data/script.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE IF NOT EXISTS project;
+
+USE project;
+
+CREATE TABLE user (
+    id INT NOT NULL AUTO_INCREMENT,
+    firstName VARCHAR(255) NOT NULL,
+    lastName VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    username VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    PRIMARY KEY(ID)
+);
