1644381ea5faa0f33c12305c59dc653799e172f7
ripwin <sathiya.kirushnapillai@heig-vd.ch>
2018-05-24 10:24:46 +0200

    Clean useless stuff

--- dev/app/controllers/UserController.scala
+++ dev/app/controllers/UserController.scala
@@ -4,9 +4,6 @@ import dao.UserDAO
 import javax.inject.{Inject, Singleton}
 import models.{LoginForm, SignUpForm, User}
 import org.mindrot.jbcrypt.BCrypt
-import play.api.libs.json._
-import play.api.libs.json.Reads.minLength
-import play.api.libs.functional.syntax._
 import play.api.mvc._
 
 import scala.concurrent.ExecutionContext.Implicits.global
@@ -16,49 +13,10 @@ import scala.concurrent.Future
 @Singleton
 class UserController @Inject()(cc: ControllerComponents, userDAO: UserDAO) extends AbstractController(cc) with play.api.i18n.I18nSupport {
 
-  // Convert a User-model object into a JsValue representation, which means that we serialize it into JSON.
-  implicit val userToJson: Writes[User] = (
-    (JsPath \ "id").write[Option[Long]] and
-      (JsPath \ "firstName").write[String] and
-      (JsPath \ "lastName").write[String] and
-      (JsPath \ "email").write[String] and
-      (JsPath \ "username").write[String] and
-      (JsPath \ "password").write[String]
-    // Use the default 'unapply' method (which acts like a reverted constructor) of the User case class if order to get
-    // back the User object's arguments and pass them to the JsValue.
-    ) (unlift(User.unapply))
-
-  // Convert a JsValue representation into a User-model object, which means that we deserialize the JSON.
-  implicit val jsonToUser: Reads[User] = (
-    (JsPath \ "id").readNullable[Long] and
-      (JsPath \ "firstName").read[String](minLength[String](2)) and
-      (JsPath \ "lastName").read[String](minLength[String](2)) and
-      (JsPath \ "email").read[String](minLength[String](2)) and
-      (JsPath \ "username").read[String](minLength[String](2)) and
-      (JsPath \ "password").read[String](minLength[String](8))
-    // Use the default 'apply' method (which acts like a constructor) of the User case class with the JsValue in order
-    // to construct a user object from it.
-    ) (User.apply _)
-
-
   // Form post action to set
   private val postJoinUrl = routes.UserController.join()
   private val postLoginUrl = routes.UserController.login()
 
-
-  def validateJson[A: Reads] = parse.json.validate(
-    _.validate[A].asEither.left.map(e => BadRequest(JsError.toJson(e)))
-  )
-
-
-  /**
-    * Get the list of all existing users, then return it.
-    * The Action.async is used because the request is asynchronous.
-    */
-  def getUsers = Action.async {
-    val userList = userDAO.list()
-    userList map (s => Ok(Json.toJson(s)))
-  }
 
   /**
     * Display the sign up form.
@@ -122,7 +80,7 @@ class UserController @Inject()(cc: ControllerComponents, userDAO: UserDAO) exten
         optionalUser map {
           case Some(u) if BCrypt.checkpw(formData.password, u.password) =>
             Redirect(routes.HomeController.index())
-            // TODO : Session and flash session
+          // TODO : Session and flash session
           case _ =>
             // Resend the form with error
             val formWithErrors = LoginForm.form.withGlobalError("User or password wrong")
